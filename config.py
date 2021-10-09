5596529610cbdb63241bc8073fdcd2b8cc940058
MishaKarliychuk <mishakarliyshuk@gmail.com>
2021-10-09 16:59:24 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+API_BOT = '2092141809:AAFv-K1GIkWzpLnZSfmmsircIhWbt91hyXI'
+ADMINS = [1526525522]
+
+TELEGRAM_CHANNEL = "https://t.me/garant4ek"
+MENEDGER = "https://t.me/garant4ek"
+
+# ПРАЦЮЄ ВЖЕ РІВНО 2 години 20 хвилин
+# ПРАЦЮЄ ВЖЕ РІВНО 2 години 14 хвилин
+# ПРАЦЮЄ ВЖЕ РІВНО 4 години 8 хвилин
+
+
